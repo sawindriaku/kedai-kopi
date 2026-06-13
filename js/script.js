507f498416adca4788217b561cfcda61283730bf
unknown <indrisawindri2@gmail.com>
2026-06-14 01:43:11 +0700

    menambahkan fitur search form

--- js/script.js
+++ js/script.js
@@ -6,10 +6,25 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik diluar sidebar untuk menghilanngkan nav
+// toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  // agar aksi default tidak dilakukan, seperti ketika ingin pindah form atau melakukan aksi yang lain tanpa mempengaruhi default awal
+  (e, preventDefault());
+};
+
+// klik diluar sidebar untuk menghilanngkan nav diluar elemen
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-btn");
+
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
