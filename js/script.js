84b9a9c73621526c7a34c58e2ccf55d4c67f1e1f
unknown <indrisawindri2@gmail.com>
2026-06-13 20:48:23 +0700

    membuat website kedai kopi

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toggle class active navbar
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar untuk menghilanngkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
